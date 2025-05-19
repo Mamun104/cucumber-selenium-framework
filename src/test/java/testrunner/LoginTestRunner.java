819d811e318116dedfb8b87a60510c56f1626f0c
Mamun104 <muhammadmamunurrashid360@gmail.com>
2025-05-19 19:03:14 +0600

    update the project

--- src/test/java/testrunner/LoginTestRunner.java
+++ src/test/java/testrunner/LoginTestRunner.java
@@ -4,28 +4,12 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-/*@RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "src/test/resources/login.feature",
-        glue = {"stepDefinitions", "setup"},
-        plugin = {
-                "pretty",
-                "html:target/cucumber-reports/cucumber.html",
-                "json:target/cucumber-reports/cucumber.json"
-        },
-        monochrome = true
-)
-
-
- */
-
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/login.feature",
         glue = {"stepDefinitions", "setup"},
         plugin = {
                 "pretty",
-                "io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm", // 👈 Allure plugin
                 "json:build/reports/cucumber.json"
         },
         monochrome = true
